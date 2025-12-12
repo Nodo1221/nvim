1ca61e6051d298ed00cfec9a0ebacffe2dec3ea2
Nodo1221 <killforskill2005@gmail.com>
2025-12-12 02:27:32 +0100

    Tokyonight

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "rust", "javascript", "c", "cpp", "latex"},
+                ensure_installed = { "rust", "javascript", "c", "cpp"},
                 auto_install = true,
                 highlight = { enable = true },
             })
@@ -25,7 +25,7 @@ require("lazy").setup({
 require("mini.pairs").setup()
 
 -- Colourscheme
-vim.cmd.colorscheme("tokyonight-storm")
+vim.cmd.colorscheme("tokyonight")
 
 --vim.g.vimtex_view_method = "zathura"        -- choose your PDF viewer
 --vim.g.vimtex_compiler_method = "latexmk"   -- default compiler
